863002f5d375d5cfeffb0dfef3b4018a3dfdfced
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-04 21:16:31 +0800

    feat: add fn for creating task

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const Form: React.FC = () => {
+  const [title, setTitle] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+
+  const createTask = async () => {
+    // title & body must have a value
+    if (!title || !body) {
+      toast.error("Please input properly!");
+      return;
+    }
+
+    const data = {
+      title,
+      body,
+    };
+
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/todos`,
+        data
+      );
+
+      if (response.status === 200) toast.success("Success!");
+      else throw new Error("Something went wrong!");
+    } catch (error) {
+      toast.error(`${error}`);
+    }
+  };
   return (
     <section className="mb-4 form-control">
       <label className="label">
@@ -12,6 +41,10 @@ const Form: React.FC = () => {
           type="text"
           placeholder="School"
           className="input input-bordered"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
         />
       </label>
       <label className="label">
@@ -23,9 +56,15 @@ const Form: React.FC = () => {
           type="text"
           placeholder="Calculus homework"
           className="input input-bordered"
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
         />
       </label>
-      <button className="m-5 btn btn-ghost">Create</button>
+      <button className="m-5 btn btn-ghost" onClick={createTask}>
+        Create
+      </button>
     </section>
   );
 };
