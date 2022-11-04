c8b209dd66ebe8c6b65023ee6288203ed59ee7a3
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-04 22:35:53 +0800

    feat: add tasks context

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
+
 import App from "./App";
+import { SharedTasksProvider } from "./lib/TaskContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-    <Toaster />
+    <SharedTasksProvider>
+      <App />
+      <Toaster />
+    </SharedTasksProvider>
   </React.StrictMode>
 );
