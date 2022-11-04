c45e7258f026e93960ce9157fd4ecd362579ad53
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-04 19:29:49 +0800

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Navbar from "./Navbar";
+import illustration from "./assets/illustration.svg";
+
 function App() {
   return (
-    <div>
-      <h1>HEY!</h1>
-    </div>
+    <main className="flex flex-col items-center min-h-screen">
+      <Navbar />
+      <img
+        className="my-5 sm:max-w-md aspect-[3/3]"
+        src={illustration}
+        alt="illustration of people having a conversation"
+      />
+    </main>
   );
 }
 
---
+++ src/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar text-neutral-content bg-neutral">
+      <a className="btn btn-ghost normal-case text-xl">Sample MERN</a>
+    </nav>
+  );
+};
+
+export default Navbar;
