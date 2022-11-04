46121bb28fcb2380d32133c5545923a58c6f9c6e
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-05 00:26:03 +0800

    feat: add update & delete functions

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,22 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import Task from "../interfaces/ITask";
 
 const Card: React.FC<Task> = (props) => {
-  const cardId = props.id;
+  const taskId = props.id;
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
 
+  const handleUpdate = async () => {
+    const data = { title, body };
+
+    const response = await axios.patch(
+      `${import.meta.env.VITE_API_URL}/todos/${taskId}`,
+      data
+    );
+
+    console.log(response.data);
+  };
+
+  const handleDelete = async () => {
+    const response = await axios.delete(
+      `${import.meta.env.VITE_API_URL}/todos/${taskId}`
+    );
+
+    console.log(response.data);
+  };
   return (
     <div className="card md:w-96 bg-base-100 shadow-xl">
       <div className="card-body">
         <input
           type="text"
-          placeholder={props.title}
+          placeholder={title}
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
           className="input font-bold w-full max-w-xs focus:outline-0"
         />
-        <textarea className="textarea" placeholder={props.body}></textarea>
+        <textarea
+          className="textarea"
+          placeholder={body}
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        ></textarea>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Update</button>
-          <button className="btn btn-error">Delete</button>
+          <button className="btn btn-primary" onClick={handleUpdate}>
+            Update
+          </button>
+          <button className="btn btn-error" onClick={handleDelete}>
+            Delete
+          </button>
         </div>
       </div>
     </div>
