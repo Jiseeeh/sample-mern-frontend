a54ca69d11c7017dd57030dc9173efe6fa0436de
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-05 13:21:32 +0800

    fix: revert to async functions
    this is for me to use loading logic easily

--- src/Card.tsx
+++ src/Card.tsx
@@ -11,7 +11,8 @@ const Card: React.FC<Task> = (props) => {
   const [body, setBody] = useState(props.body);
   const [, setTasks] = useTasks();
 
-  const handleUpdate = () => {
+  const handleUpdate = async () => {
+    const toastId = toast.loading("Updating your task in the cloud");
     const data = { title, body };
 
     setTasks((prevTaskVal) => {
@@ -24,32 +25,32 @@ const Card: React.FC<Task> = (props) => {
       return prevTaskVal;
     });
 
-    const response = axios.patch(
+    const response = await axios.patch(
       `${import.meta.env.VITE_API_URL}/todos/${taskId}`,
       data
     );
 
-    toast.promise(response, {
-      loading: "Updating your task in the cloud",
-      error: "Something went wrong!",
-      success: "Update success!",
-    });
+    if (response.status === 200) {
+      toast.dismiss(toastId);
+      toast.success("Update success");
+    }
   };
 
-  const handleDelete = () => {
+  const handleDelete = async () => {
+    const toastId = toast.loading("Syncing delete in the cloud");
+
     setTasks((prevTasksVal) =>
       prevTasksVal.filter((task) => task.id !== taskId)
     );
 
-    const response = axios.delete(
+    const response = await axios.delete(
       `${import.meta.env.VITE_API_URL}/todos/${taskId}`
     );
 
-    toast.promise(response, {
-      loading: "Syncing delete in the cloud",
-      error: "Something went wrong!",
-      success: "Delete success!",
-    });
+    if (response.status === 200) {
+      toast.dismiss(toastId);
+      toast.success("Delete success");
+    }
   };
 
   return (
